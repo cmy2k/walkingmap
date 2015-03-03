a55adf8a3f230e5ea23faec6f605d59f131490a0
Jeff Hicks <jmhconsultingllc@gmail.com>
2015-03-03 10:51:22 -0500

    better markers

--- geo.js
+++ geo.js
@@ -1,3 +1,5 @@
+L.AwesomeMarkers.Icon.prototype.options.prefix = 'ion';
+
 function makeMarkersFromLegs(path, legs, map) {
   var markers = [];
 
@@ -9,13 +11,42 @@ function makeMarkersFromLegs(path, legs, map) {
   // find location of end points along path
   var distance = 0;
   var markers = [];
+
+  // add start and end markers
+  var startMarker = L.AwesomeMarkers.icon({
+    icon: 'play',
+    markerColor: 'green'
+  });
+
+  var startCoordinates = L.GeometryUtil.interpolateOnLine(map, polypath, 0).latLng;
+
+  markers.push(L.marker([startCoordinates.lng, startCoordinates.lat], {icon: startMarker}));
+
+  var endMarker = L.AwesomeMarkers.icon({
+    icon: 'ribbon-a',
+    markerColor: 'orange'
+  });
+
+  var endCoordinates = L.GeometryUtil.interpolateOnLine(map, polypath, 1).latLng;
+
+  markers.push(L.marker([endCoordinates.lng, endCoordinates.lat ], {icon: endMarker}));
+
   _.forEach(legs[0], function(leg) {
     if (leg.dist !== 0) {
       distance += leg.dist;
       var ratio = distance / length;
       var location = L.GeometryUtil.interpolateOnLine(map, polypath, ratio).latLng;
-      var marker = L.marker([location.lng, location.lat]);
-      marker.bindPopup("<b>" + leg.date + "</b><br>Distance: " + leg.dist + " miles<br>Calories burned: " + leg.cals)
+      var marker = L.marker([location.lng, location.lat], {icon: L.AwesomeMarkers.icon({
+        icon: 'android-walk', markerColor: 'blue'
+      })});
+      var popup = "".concat(
+          "<b>", 
+          leg.date, 
+          "</b><br>Distance: ", 
+          leg.dist, 
+          " miles<br>Calories burned: ",
+          leg.cals);
+      marker.bindPopup(popup);
       markers.push(marker);
     }
   });
